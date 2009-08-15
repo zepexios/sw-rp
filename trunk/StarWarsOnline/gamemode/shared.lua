817e762d5a680e61296a074aaba50fb5565d0cc1
meeces2911@gmail.com <meeces2911@gmail.com@e4b46e76-4723-11de-aa63-c184e82abc01>
2009-08-15 05:21:37 +0000

    Changed Clientside loading system to be recursive. ie, now loads folders within the base dir (commented out redundant loads in character)

--- trunk/StarWarsOnline/gamemode/shared.lua
+++ trunk/StarWarsOnline/gamemode/shared.lua
@@ -9,23 +9,45 @@ function SWO.Debug( Text )
 	Msg( "SWO - ", Text.."\n" )
 end
 
+function SWO.LoadDirRecur(base, client)
+	SWO.LoadDir(base, client) -- call itsself once to load the FILES inside the current dir, then recur, to load the other dirs!
+	for _,folder in pairs(file.FindDir("../Gamemodes/"..GM.FolderName .. "/gamemode/" .. base .. "*")) do
+		for _, thefile in pairs(file.FindInLua( GM.FolderName .. "/gamemode/" .. base .. folder .. "/" .. "*")) do
+			--I wound advise not to use string.find, i would use string.sub and check for a perticular file extension
+			--if string.find(thefile, ".lua", 1, true) != nil then
+			local name = string.sub(thefile,-3)
+			if ( name == "lua") then
+				SWO.Debug("Loading: "..thefile)
+				if client then
+					AddCSLuaFile( base .. folder .. "/" .. thefile)
+				else
+					include( base .. folder .. "/" .. thefile)
+				end
+			end
+			if (name == "mp3" or name == "wav") then
+				if client then SWO.Debug("Client Loaded Sound Files") return end
+				SWO.Debug("Adding Sound: "..thefile)
+				resource.AddFile(GM.FolderName .. "/gamemode/" .. base .. folder .. "/" .. thefile)
+			end
+		end
+	end
+end
+
 function SWO.LoadDir(base, client)
-	for i, thefile in pairs(file.FindInLua("../" .. GM.Folder .. "/gamemode/" .. base .. "*")) do
-		--I wound advise not to use string.find, i would use string.sub and check for a perticular file extension
-		--if string.find(thefile, ".lua", 1, true) != nil then
+	for _, thefile in pairs(file.FindInLua( GM.FolderName .. "/gamemode/" .. base .. "*")) do
 		local name = string.sub(thefile,-3)
 		if ( name == "lua") then
 			SWO.Debug("Loading: "..thefile)
 			if client then
-				AddCSLuaFile(GM.FolderName.."/gamemode/" .. base .. thefile)
+				AddCSLuaFile( base .. thefile)
 			else
-				include(GM.FolderName.."/gamemode/" .. base .. thefile)
+				include( base .. thefile)
 			end
 		end
 		if (name == "mp3" or name == "wav") then
 			if client then SWO.Debug("Client Loaded Sound Files") return end
 			SWO.Debug("Adding Sound: "..thefile)
-			resource.AddFile(GM.FolderName.."/gamemode/" .. base .. thefile)
+			resource.AddFile(GM.FolderName .. "/gamemode/" .. base .. thefile)
 		end
 	end
 end
