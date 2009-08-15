96b1c44f314d901caa8ace70fe81ea02f99d8e19
meeces2911@gmail.com <meeces2911@gmail.com@e4b46e76-4723-11de-aa63-c184e82abc01>
2009-08-15 07:11:12 +0000

    Changed loading system again, as i have seen what Revan was trying to do in the first place. Changed the mouse press command to regular command rather than toggle

--- trunk/StarWarsOnline/gamemode/shared.lua
+++ trunk/StarWarsOnline/gamemode/shared.lua
@@ -9,32 +9,10 @@ function SWO.Debug( Text )
 	Msg( "SWO - ", Text.."\n" )
 end
 
-function SWO.LoadDirRecur(base, client)
-	SWO.LoadDir(base, client) -- call itsself once to load the FILES inside the current dir, then recur, to load the other dirs!
-	for _,folder in pairs(file.FindDir("../Gamemodes/"..GM.FolderName .. "/gamemode/" .. base .. "*")) do
-		for _, thefile in pairs(file.FindInLua( GM.FolderName .. "/gamemode/" .. base .. folder .. "/" .. "*")) do
-			--I wound advise not to use string.find, i would use string.sub and check for a perticular file extension
-			--if string.find(thefile, ".lua", 1, true) != nil then
-			local name = string.sub(thefile,-3)
-			if ( name == "lua") then
-				SWO.Debug("Loading: "..thefile)
-				if client then
-					AddCSLuaFile( base .. folder .. "/" .. thefile)
-				else
-					include( base .. folder .. "/" .. thefile)
-				end
-			end
-			if (name == "mp3" or name == "wav") then
-				if client then SWO.Debug("Client Loaded Sound Files") return end
-				SWO.Debug("Adding Sound: "..thefile)
-				resource.AddFile(GM.FolderName .. "/gamemode/" .. base .. folder .. "/" .. thefile)
-			end
-		end
-	end
-end
-
 function SWO.LoadDir(base, client)
 	for _, thefile in pairs(file.FindInLua( GM.FolderName .. "/gamemode/" .. base .. "*")) do
+		--I wound advise not to use string.find, i would use string.sub and check for a perticular file extension
+		--if string.find(thefile, ".lua", 1, true) != nil then
 		local name = string.sub(thefile,-3)
 		if ( name == "lua") then
 			SWO.Debug("Loading: "..thefile)
@@ -47,7 +25,7 @@ function SWO.LoadDir(base, client)
 		if (name == "mp3" or name == "wav") then
 			if client then SWO.Debug("Client Loaded Sound Files") return end
 			SWO.Debug("Adding Sound: "..thefile)
-			resource.AddFile(GM.FolderName .. "/gamemode/" .. base .. thefile)
+			resource.AddFile(GM.FolderName .. "/gamemode/" .. base .. folder .. "/" .. thefile)
 		end
 	end
 end
